67466582ff9ead9d6139e5acd0ff1c8485f1da97
skagra_dragneel <nillocollin2012@gmail.com>
2016-12-03 21:23:36 -0500

    Finish dead zone

--- 333x/advancedAttempt.c
+++ 333x/advancedAttempt.c
@@ -19,13 +19,51 @@
 	arm motion
 	arm lock control
 */
-#define C1LX vexRT[Ch4]
-#define C1LY vexRT[Ch3]
-#define C1RX vexRT[Ch1]
+
 
 int lockArmPosition = nMotorEncoder[topRight];
 int additionPower = 0;
 
+//Gets Joystick Values with dead zone
+int * getJoystick()
+{
+	int joystickValues[] = {0,0,0};
+	int minVal = 30;
+
+	//Geting C1LX or ch4
+	int currentVal = abs(vexRT[Ch4]);
+	if(currentVal >= minVal)
+	{
+		joystickValues[0] = vexRT[Ch4];
+	}
+	else
+	{
+		joystickValues[0]= 0;
+	}
+
+	//Getting C1LY of ch3
+	currentVal = abs(vexRT[Ch3]);
+	if(currentVal >= minVal)
+	{
+		joystickValues[1] = vexRT[Ch3];
+	}
+	else
+	{
+		joystickValues[1] = 0;
+	}
+
+	//Getting C1Rx or ch1
+	currentVal = abs(vexRT[Ch1]);
+	if(currentVal >= minVal)
+	{
+		joystickValues[2] = vexRT[Ch1];
+	}
+	else
+	{
+		joystickValues[2] = 0;
+	}
+	return joystickValues;
+}
 
 void moveShooter(int speed)
 {
@@ -42,15 +80,16 @@ void lockArm()
 	{
 		additionPower += 2; //Increment by 2
 	}
-	else if(lockArmPosition < nMotorEncoder[topRight] - 3)
-	{
-		additionPower -=2;
-	}
 	moveShooter(additionPower); //Zero if locked it on its own
 }
 
 void driveControl()
 {
+	int *joystickValues = getJoystick();
+	int C1LX = joystickValues[0];
+	int C1LY = joystickValues[1];
+	int C1RX = joystickValues[2];
+
 	motor[FR] = C1LX + C1RX;
 	motor[LE] =  -C1LY + C1RX;
 	motor[RI] =  C1LY + C1RX;
