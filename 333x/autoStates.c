9e3beb0b487ec564d8b817cf808395225c1d59d7
skagra_dragneel <nillocollin2012@gmail.com>
2017-02-03 17:02:46 -0500

    Add basic outline for auto

---
+++ 333x/autoStates.c
@@ -0,0 +1,46 @@
+#pragma config(I2C_Usage, I2C1, i2cSensors)
+#pragma config(Sensor, in1,    rightPot,       sensorPotentiometer)
+#pragma config(Sensor, in2,    leftPot,        sensorPotentiometer)
+#pragma config(Sensor, in3,    autoControl,    sensorNone)
+#pragma config(Sensor, dgtl1,  autoSensor,     sensorTouch)
+#pragma config(Sensor, dgtl2,  bottomLimit,    sensorNone)
+#pragma config(Sensor, dgtl3,  autoSensor,     sensorNone)
+#pragma config(Sensor, I2C_1,  ,               sensorQuadEncoderOnI2CPort,    , AutoAssign )
+#pragma config(Sensor, I2C_2,  ,               sensorQuadEncoderOnI2CPort,    , AutoAssign )
+#pragma config(Sensor, I2C_3,  ,               sensorQuadEncoderOnI2CPort,    , AutoAssign )
+#pragma config(Motor,  port1,           rightPincer,   tmotorVex393_HBridge, openLoop)
+#pragma config(Motor,  port2,           bottomRight,   tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port3,           topLeft,       tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port4,           frontRight,    tmotorVex393_MC29, openLoop, reversed)
+#pragma config(Motor,  port5,           frontLeft,     tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port6,           backRight,     tmotorVex393_MC29, openLoop, reversed, encoderPort, I2C_1)
+#pragma config(Motor,  port7,           backLeft,      tmotorVex393_MC29, openLoop, encoderPort, I2C_2)
+#pragma config(Motor,  port8,           bottomLeft,    tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port9,           topRight,      tmotorVex393_MC29, openLoop, reversed, encoderPort, I2C_3)
+#pragma config(Motor,  port10,          leftPincer,    tmotorVex393_HBridge, openLoop)
+
+#include "autoInclude.h"
+/*
+	Collin Bolles
+	2/3/2017
+	odometry
+	Seperate Include File
+	Calculating Stopping Distance
+*/
+task main()
+{
+	calculateStoppingDistance();
+	clearTimer(T2);
+	startTask(odometry);
+	setToDefault();
+	//Select
+	if(SensorValue[autoSensor])
+	{
+		backStars();
+	}
+	else
+	{
+		basicAuto();
+	}
+	writeDebugStreamLine("Total_time_takes: %d",time1(T2));
+}
